8fe7cbfc17737c51b958ce36ecbf6de7ee4e997a
iamrazo <fake@fake.com>
2024-01-04 22:54:38 +0800

    feature: 调整

--- src/worker.js
+++ src/worker.js
@@ -6,6 +6,7 @@ export default {
 			case '/health':
 				return new Response(JSON.stringify(request.cf, null, 2), { status: 200 });
 			default: {
+				return new Response(JSON.stringify(request.cf, null, 2), { status: 200 });
 				if (url.protocol === 'http:') {
 					Object.assign(url, {
 						protocol: 'https:',
