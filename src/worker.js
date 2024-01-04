728ad52df385e59cabcf50c21006b492088e2b8e
iamrazo <fake@fake.com>
2024-01-04 19:48:12 +0800

    feature: 添加health检测

--- src/worker.js
+++ src/worker.js
@@ -2,13 +2,19 @@ const hostnames = ['github.com'];
 export default {
 	async fetch(request) {
 		const url = new URL(request.url);
-		if (url.protocol === 'http:') {
-			Object.assign(url, {
-				protocol: 'https:',
-				port: 443,
-			});
+		switch (url.pathname) {
+			case '/health':
+				return new Response(JSON.stringify(request.cf, null, 2), { status: 200 });
+			default: {
+				if (url.protocol === 'http:') {
+					Object.assign(url, {
+						protocol: 'https:',
+						port: 443,
+					});
+				}
+				url.hostname = hostnames[Math.floor(Math.random() * hostnames.length)];
+				return await fetch(new Request(url, request));
+			}
 		}
-		url.hostname = hostnames[Math.floor(Math.random() * hostnames.length)];
-		return await fetch(new Request(url, request));
 	},
 };
